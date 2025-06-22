aed7587fece6622124ccfc898ed21e30187ec605
Michael Stally Beleño Ortega <91377632+stally-ortega@users.noreply.github.com>
2025-06-22 06:47:34 +0000

    feat: agregar ruta para el componente de rendimiento del modelo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,5 +4,9 @@ export const routes: Routes = [
     {
         path: 'financial-performance',
         loadComponent: () => import('@features/financial-performance/financial-performance.component').then(m => m.FinancialPerformanceComponent)
+    },
+    {
+        path: 'model-performance',
+        loadComponent: () => import('@features/model-perfomance/model-perfomance.component').then(m => m.ModelPerfomanceComponent)
     }
 ];
